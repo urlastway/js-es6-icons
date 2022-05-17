9a59954d6fcaa29038e4bd3629cd6e7d713de6e3
urlastway <andriy.semenchenko13@gmail.com>
2022-05-17 23:46:03 +0200

    7

--- data.js
+++ data.js
@@ -113,8 +113,6 @@ const icons = [
 	}
 ];
 
-console.log(icons);
-
 const containerHTML = document.querySelector("#container");
 const tplIconsHTML = document.querySelector("#tpl-icons").content;
 
@@ -123,5 +121,16 @@ for(let i = 0; i< icons.length; i++){
 	const {name, prefix, type, family, color} = icons[i];
 	iconsHTML.querySelector(".icon").innerHTML = `<i class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"><i>`
 	iconsHTML.querySelector(".name").innerHTML = name;
+
 	containerHTML.append(iconsHTML); 
-}
+};
+
+const purple = icons.filter((elm) => elm.color == "blue");
+console.log(purple); 
+
+const gialli = icons.filter((elm) => elm.color == "green");
+console.log(gialli); 
+
+const blu = icons.filter((elm) => elm.color == "orange");
+console.log(blu);
+
